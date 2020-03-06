680dc893ffffb0cc5179a00f0c7d8af4eefe019c
PanikSchale2001 <jeriel.frei@projectix.ch>
2020-03-06 11:39:39 +0100

    created Classes

--- app/src/main/java/ninja/jfr/travelswiss/City.java
+++ app/src/main/java/ninja/jfr/travelswiss/City.java
@@ -1,4 +1,38 @@
 package ninja.jfr.travelswiss;
 
 public class City {
+
+    private String name;
+    private double xPos;
+    private double yPos;
+
+    public City(String name, double xPos, double yPos) {
+        this.name = name;
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getxPos() {
+        return xPos;
+    }
+
+    public void setxPos(double xPos) {
+        this.xPos = xPos;
+    }
+
+    public double getyPos() {
+        return yPos;
+    }
+
+    public void setyPos(double yPos) {
+        this.yPos = yPos;
+    }
 }
--- app/src/main/java/ninja/jfr/travelswiss/Weather.java
+++ app/src/main/java/ninja/jfr/travelswiss/Weather.java
@@ -1,4 +1,77 @@
 package ninja.jfr.travelswiss;
 
 public class Weather {
+    private int id;
+    private City city;
+    private String weather;
+    private String description;
+    private double temperatur;
+    private double windspeed;
+    private double rainPerHour;
+
+    public Weather(int id, City city, String weather, String description, double temperatur, double windspeed, double rainPerHour) {
+        this.id = id;
+        this.city = city;
+        this.weather = weather;
+        this.description = description;
+        this.temperatur = temperatur;
+        this.windspeed = windspeed;
+        this.rainPerHour = rainPerHour;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public String getWeather() {
+        return weather;
+    }
+
+    public void setWeather(String weather) {
+        this.weather = weather;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getTemperatur() {
+        return temperatur;
+    }
+
+    public void setTemperatur(double temperatur) {
+        this.temperatur = temperatur;
+    }
+
+    public double getWindspeed() {
+        return windspeed;
+    }
+
+    public void setWindspeed(double windspeed) {
+        this.windspeed = windspeed;
+    }
+
+    public double getRainPerHour() {
+        return rainPerHour;
+    }
+
+    public void setRainPerHour(double rainPerHour) {
+        this.rainPerHour = rainPerHour;
+    }
 }
