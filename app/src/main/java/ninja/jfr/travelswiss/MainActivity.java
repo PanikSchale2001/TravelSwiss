2b09af9bd23a2ca1bebae14d0d85452e73bdfe1f
Daniel Löffler <danlff@az.zli.ch>
2020-03-12 16:29:26 +0100

    end of day

--- app/src/main/java/ninja/jfr/travelswiss/MainActivity.java
+++ app/src/main/java/ninja/jfr/travelswiss/MainActivity.java
@@ -1,5 +1,7 @@
 package ninja.jfr.travelswiss;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
@@ -9,9 +11,11 @@ import android.content.Intent;
 import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -55,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        addTolist("a","b", "c", "c");
 
         TextView date = findViewById(R.id.textDate);
         date.setOnClickListener(new View.OnClickListener() {
@@ -150,18 +155,37 @@ public class MainActivity extends AppCompatActivity {
 
     public void addTolist(String from, String to, String time, String date) {
 
-        final ListView listview = findViewById(R.id.listview);
-        Future<List<List<Connection>>> list = {new ConnectionService().getAllConnections(from, to, time, date)};
+        final ListView listview = (ListView) findViewById(R.id.listview);
 
-        final StableArrayAdapter adapter = new StableArrayAdapter(this,
-                android.R.layout.simple_list_item_1, list);
+        ConnectionService connectionService = new ConnectionService();
+        List<List<Connection>> listOfConnection = (List<List<Connection>>) connectionService.getAllConnections(from, to, time, date);
+
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile" };
+
+
+        final List<List<Connection>> connectionList = new ArrayList<>();
+
+        for (int i = 0; i< listOfConnection.size(); i++) {
+            //the same
+        }
+
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+
+
+        final StableArrayAdapter adapter = new StableArrayAdapter(this, android.R.layout.simple_list_item_1, list);
         listview.setAdapter(adapter);
 
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
             @Override
-            public void onItemClick(AdapterView<?> parent, final View view,
-                                    int position, long id) {
+            public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
                 final String item = (String) parent.getItemAtPosition(position);
                 view.animate().setDuration(2000).alpha(0).withEndAction(new Runnable() {
                     @Override
@@ -172,14 +196,10 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
             }
-
         });
     }
 
-
-
     private class StableArrayAdapter extends ArrayAdapter<String> {
-
         HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
 
         public StableArrayAdapter(Context context, int textViewResourceId,
@@ -194,6 +214,22 @@ public class MainActivity extends AppCompatActivity {
         public long getItemId(int position) {
             String item = getItem(position);
             return mIdMap.get(item);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = inflater.inflate(R.layout.fragment_connection, parent, false);
+            TextView from = view.findViewById(R.id.from);
+            TextView to = view.findViewById(R.id.to);
+            TextView fromTime = view.findViewById(R.id.fromTime);
+            TextView toTime = view.findViewById(R.id.toTime);
+            TextView travelTime = view.findViewById(R.id.travelTime);
+            TextView fromTrack = view.findViewById(R.id.fromTrack);
+            TextView toTrack = view.findViewById(R.id.toTrack);
+
+            return super.getView(position, convertView, parent);
         }
 
         @Override
